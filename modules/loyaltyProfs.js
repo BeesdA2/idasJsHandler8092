00f772f69871535b3441a5ec5b6f64f32f17954f
mvmeel05 <m.van.meel@beesda2.nl>
2025-06-26 16:36:56 +0200

    Upload toegevoegd

--- modules/loyaltyProfs.js
+++ modules/loyaltyProfs.js
@@ -1,9 +1,10 @@
 const { downloadLaatsteBestandLoyaltyProfs } = require("../../../../Productie/loyaltyProfs/js/consumeLoyaltyProfsSFTP.js");
+const { uploadTSVBestandLoyaltyProfs } = require("../../../../Productie/loyaltyProfs/js/consumeLoyaltyProfsSFTP.js");
 
 
  
 
-async function consumeLoyaltyProfs  (req, res)  {
+async function consumeLoyaltyProfsDownload  (req, res)  {
   
    
   let host = req.query.host;
@@ -28,8 +29,35 @@ async function consumeLoyaltyProfs  (req, res)  {
   
 };
 
+async function consumeLoyaltyProfsUpload  (req, res)  {
+  
+   
+  let host = req.query.host;
+  let port = req.query.port;
+  let username = req.query.username;
+  let password = req.query.password; 
+  let sftpFileName = req.query.sftpFileName;
+  
+  console.log(sftpFileName);
+  
+    
+ var d = new Date();
+ var datetime = d.toLocaleString(); 	
+ console.log(datetime + ' consumeLoyaltyProfs host: ' +host + ' port: ' + port + ' username: ' +username );
+ 
+
+
+
+  var resolveWebservice =  await uploadTSVBestandLoyaltyProfs(req, res);
+   
+  let responseResolveWebservice = await resolveWebservice;
+  
+  return responseResolveWebservice;
+  
+};
 
 
 module.exports = {
-  consumeLoyaltyProfs: consumeLoyaltyProfs
+  consumeLoyaltyProfsDownload: consumeLoyaltyProfsDownload,
+  consumeLoyaltyProfsUpload: consumeLoyaltyProfsUpload,
   };
